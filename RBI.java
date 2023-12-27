dd10521ac91be4d784026501d5a54600a7cf54c8
ANKIT DILIP GAIKWAD <143960237+Ankitdg321@users.noreply.github.com>
2023-12-27 15:13:25 +0530

    Create RBI.java

---
+++ RBI.java
@@ -0,0 +1,11 @@
+package com.tcs.bankapplication.service;
+
+public interface RBI {
+
+	public void registerAccount();
+	public void showAccountDetails();
+	public void showBalance();
+	public void withdraw();
+	public void Deposit();
+	public void UpdateAccountDetails();
+}
